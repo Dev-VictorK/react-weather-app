11615f48dba31583a95860bb50c28f16c4131e46
Victor Bundi <victorbundi.dev@gmail.com>
2025-07-15 16:35:13 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+{/**Weather App
+
+Fetch weather from an API like OpenWeatherMap.
+
+Display temperature, location, and icon. */}
+function App() {
+  return (
+    <div className="App">
+      Weather App
+    </div>
+  );
+}
+
+export default App;
